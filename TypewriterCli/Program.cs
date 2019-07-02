83cc5ae1f45cdcea3b34bd3333d306d7a49fe8df
Gbemiga Meduoye <bem.meduoye@reddeer.com>
2019-07-02 01:52:24 +0100

    made main public

--- TypewriterCli/Program.cs
+++ TypewriterCli/Program.cs
@@ -11,7 +11,7 @@ namespace TypewriterCli
 {
     public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //var stopwatch = Stopwatch.StartNew();
             var showHelp = args == null || args.Length == 0;
