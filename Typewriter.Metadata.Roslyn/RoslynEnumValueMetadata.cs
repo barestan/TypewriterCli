6d0bca4a449155275d5000bf6cfe75f7d04ae8cc
Gbemiga Meduoye <bem.meduoye@reddeer.com>
2019-07-04 03:21:57 +0100

    Patched issue: Generator blowing up when enums have constant values

--- Typewriter.Metadata.Roslyn/RoslynEnumValueMetadata.cs
+++ Typewriter.Metadata.Roslyn/RoslynEnumValueMetadata.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Typewriter.Metadata.Interfaces;
 
 namespace Typewriter.Metadata.Roslyn
@@ -11,6 +13,8 @@ namespace Typewriter.Metadata.Roslyn
         private static readonly Int64Converter Converter = new Int64Converter();
 
         private readonly IFieldSymbol _symbol;
+        
+        private Regex CONSTANT_VALUE_SYNTAX  = new Regex(@"\w+\s*=\s*(\d+)");
 
         private RoslynEnumValueMetadata(IFieldSymbol symbol)
         {
@@ -21,7 +25,22 @@ namespace Typewriter.Metadata.Roslyn
         public string Name => _symbol.Name;
         public string FullName => _symbol.ToDisplayString();
         public IEnumerable<IAttributeMetadata> Attributes => RoslynAttributeMetadata.FromAttributeData(_symbol.GetAttributes());
-        public long Value => (long?)Converter.ConvertFromString(_symbol.ConstantValue.ToString().Trim('\'')) ?? -1;
+        public long Value => (long?)Converter.ConvertFromString( TryGetConstantValue()) ?? -1;
+
+        private string TryGetConstantValue()
+        {
+            if (_symbol.ConstantValue == null)
+            {
+                var node = _symbol.GetType().GetProperty("SyntaxNode", typeof(EnumMemberDeclarationSyntax));
+                var propertyText = node.GetValue(_symbol) + "";
+                if (propertyText != null && CONSTANT_VALUE_SYNTAX.IsMatch(propertyText))
+                {
+                    var value = CONSTANT_VALUE_SYNTAX.Match(propertyText);
+                    return value.Groups[1] + "";
+                }
+            }
+            return _symbol.ConstantValue.ToString().Trim('\'');
+        }
 
         internal static IEnumerable<IEnumValueMetadata> FromFieldSymbols(IEnumerable<IFieldSymbol> symbols)
         {
