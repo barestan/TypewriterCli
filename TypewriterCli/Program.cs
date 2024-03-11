5fb78318cc8fadb4cdc93fd5b75b1958016afd41
Pablo F D <pablo@algosimple.fr>
2024-03-11 16:31:14 +0100

    Use the real Main entry point and not the one for local tests.

--- TypewriterCli/Program.cs
+++ TypewriterCli/Program.cs
@@ -16,7 +16,7 @@ namespace TypewriterCli
     public static class Program
     {
         
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
             //var dtoInputPath = Path.Combine(@"C:\Development\Firefly.Backbone\Service\Calculator\Risk\Risk.Shared\Dtos");
             var templatePath = Path.Combine(@"C:\Development\Firefly.Backbone\Client\Client", "Api", "_TypeWriterTemplate.tst");
@@ -24,7 +24,7 @@ namespace TypewriterCli
             cliArgs.Recursive = true;
             Generate(cliArgs);
         }
-        public static void Main2(string[] args)
+        public static void Main(string[] args)
         {
             //var stopwatch = Stopwatch.StartNew();
             var showHelp = args == null || args.Length == 0;
